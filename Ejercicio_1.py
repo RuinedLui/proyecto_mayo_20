8fba0a268d526c1143ed3f132b236622f6ecce1c
Gerard AC <gerardoac249@gmail.com>
2026-05-21 19:00:39 -0600

    ejercicio modelo analitico de recursos humanos

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -29,3 +29,49 @@ for i in range(1, 36):
 df_empleados = pd.DataFrame(empleados)
 
 print(df_empleados.head())
+
+df_empleados["salario"] = [
+    random.randint(500, 3000) for _ in range(len(df_empleados))
+]
+
+print(df_empleados.head())
+
+df_final = pd.merge(
+    df_empleados,
+    departamentos,
+    on="departamento_id"
+)
+
+print(df_final.head())
+
+df_final = df_final.rename(columns={
+    "nombre_x": "empleado",
+    "nombre_y": "departamento"
+})
+
+print(df_final.head())
+
+promedio_salario = df_final["salario"].mean()
+
+print("Salario promedio general:")
+print(promedio_salario)
+
+salario_maximo = df_final["salario"].max()
+
+print("Salario máximo:")
+print(salario_maximo)
+
+promedio_departamento = df_final.groupby(
+    "departamento"
+)["salario"].mean()
+
+print("Promedio salarial por departamento:")
+print(promedio_departamento)
+
+promedio_departamento.plot(kind="bar")
+
+plt.title("Salario Promedio por Departamento")
+plt.xlabel("Departamento")
+plt.ylabel("Salario Promedio")
+
+plt.show()
